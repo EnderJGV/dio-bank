eb9797a38cf2fba422a33e1474e1be99ccf8f02c
Ender Guaina <ender.guaina@gmail.com>
2026-05-07 18:08:58 -0300

    feature: creando los arquivos para os modelos

--- src/app.py
+++ src/app.py
@@ -57,7 +57,7 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
-        SQLALCHEMY_DATABASE_URI=os.environ["DATABASE_URL"],
+        SQLALCHEMY_DATABASE_URI="sqlite:///blog.sqlite",
         JWT_SECRET_KEY="super-secret",
     )
 
