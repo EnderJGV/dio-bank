08f5d7dfe61a4425f036722b933a633351bc3c38
Ender Guaina <ender.guaina@gmail.com>
2026-05-10 19:36:35 -0300

    subindo todos os conteudos aprendidos com flask

--- src/app.py
+++ src/app.py
@@ -4,13 +4,26 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+from flask_marshmallow import Marshmallow
+
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+from apispec_webframeworks.flask import FlaskPlugin
+
 
 from src.models import db, Post, Role, User
 
 migrate = Migrate()
 jwt = JWTManager()
-
 bcrypt = Bcrypt()
+ma = Marshmallow()
+spec = APISpec(
+    title="Dio Bank",
+    version="1.0.0",
+    openapi_version="3.0.4",
+    info=dict(description="DIO Bank API"),
+    plugins=[FlaskPlugin(), MarshmallowPlugin()],
+)
 
 def create_app(environment=os.environ["ENVIRONMENT"]):
     # create and configure the app
@@ -23,11 +36,12 @@ def create_app(environment=os.environ["ENVIRONMENT"]):
     # def hello():
     #     return 'Hello, World!'
 
+
     db.init_app(app)
     migrate.init_app(app, db)
     jwt.init_app(app)
     bcrypt.init_app(app)
-
+    ma.init_app(app)
     # Register BluePrint
     from src.controllers import user, post, auth, role
 
@@ -35,5 +49,35 @@ def create_app(environment=os.environ["ENVIRONMENT"]):
     app.register_blueprint(post.app)
     app.register_blueprint(auth.app)
     app.register_blueprint(role.app)
+
+    @app.route("/docs")
+    def docs():
+        spec.path(view=user.handle_user)
+        spec.path(view=user.get_user)
+        spec.path(view=user.update_user)
+        spec.path(view=user.delete_user)
+        spec.path(view=post.create_post)
+        spec.path(view=post.get_posts)
+        spec.path(view=post.update_post)
+        spec.path(view=post.delete_post)
+        spec.path(view=role.create_role)
+        return spec.to_dict()
+
+    from flask import json
+    from werkzeug.exceptions import HTTPException
+
+    @app.errorhandler(HTTPException)
+    def handle_exception(e):
+        """Return JSON instead of HTML for HTTP errors."""
+        # start with the correct headers and status code from the error
+        response = e.get_response()
+        # replace the body with JSON
+        response.data = json.dumps({
+            "code": e.code,
+            "name": e.name,
+            "description": e.description,
+        })
+        response.content_type = "application/json"
+        return response
 
     return app
